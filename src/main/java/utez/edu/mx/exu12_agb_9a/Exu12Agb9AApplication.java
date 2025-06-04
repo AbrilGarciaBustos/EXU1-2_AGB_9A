49198a1d9f2e8a602e1a826cf64fe6d9c74c70b3
AbrilGarciaBustos <20223tn053@utez.edu.mx>
2025-06-03 18:26:37 -0600

    [FIRST COMMIT] Archivo actividad 1

---
+++ src/main/java/utez/edu/mx/exu12_agb_9a/Exu12Agb9AApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.exu12_agb_9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exu12Agb9AApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exu12Agb9AApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/exu12_agb_9a/Exu12Agb9AApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.exu12_agb_9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exu12Agb9AApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
